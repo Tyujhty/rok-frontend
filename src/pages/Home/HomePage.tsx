e2c34cf0cee7ddd1ecadec2c5247bc6d8a1542d6
Tyujhty <fanny.baquey@protonmail.com>
2024-07-24 16:42:15 +0200

    Update docker environment prod and dev.

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <>
       <div className="w-full flex justify-center items-center mt-8">
         <CardBody>
-          <h2>Lorem</h2>
+          <h2>Lorem Ipsum</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut,
             praesentium ut. Perferendis iure, iste nam tempore et illo nihil
